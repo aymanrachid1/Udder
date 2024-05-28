5eba7bac65bd68ba7fa20504201b90b41cf8bd10
Ayman Rachid <aymanrachid2008@gmail.com>
2024-05-29 06:54:00 +0930

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Ayman Rachid on 29/5/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
